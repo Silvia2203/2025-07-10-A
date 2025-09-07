3ea442ab686d2dbac60145549d34ec3a29e27463
Silvia2203 <daniello.silvia22@gmail.com>
2025-09-07 15:07:49 +0200

    punto 1

--- UI/controller.py
+++ UI/controller.py
@@ -10,13 +10,38 @@ class Controller:
         # the model, which implements the logic of the program and holds the data
         self._model = model
 
-    def handleCreaGrafo(self, e):
-        pass
+    def fillDDCat(self):
+        cat = self._model.getCategories()
+        for c in cat:
+            self._view._ddcategory.options.append(ft.dropdown.Option(text=c.category_name,
+                                                                  data=c))
+        self._view.update_page()
 
-    def handleBestProdotti(self, e):
-        pass
+    def handleCreaGrafo(self, e):
+        cat = self._view._ddcategory.value
+        self._model.buildGraph(cat, self._view._dp1.value, self._view._dp2.value)
+        Nnodes, Nedges = self._model.getGraphDetails()
+        self._view.txt_result.controls.clear()
+        self._view.txt_result.controls.append(ft.Text("Date selezionate:"))
+        self._view.txt_result.controls.append(ft.Text(f"Start date: {self._view._dp1.value.date()}"))
+        self._view.txt_result.controls.append(ft.Text(f"End date: {self._view._dp2.value.date()}"))
+        self._view.txt_result.controls.append(ft.Text("Grafo correttamente creato:"))
+        self._view.txt_result.controls.append(ft.Text(f"Numero di nodi:{Nnodes}"))
+        self._view.txt_result.controls.append(ft.Text(f"Numero di archi:{Nedges}"))
+        self._fillDDProds()
+        self._view.update_page()
+
+    def handleBestProdotti(self,e):
+        bestprodotti = self._model.getBestProdotti()
+        self._view.txt_result.controls.append(ft.Text(f"I cinque prodotti più venduti sono:"))
+        for p in bestprodotti:
+            self._view.txt_result.controls.append(ft.Text(f"{p[0].product_name} with score {p[1]}"))
+        self._view.update_page()
 
     def handleCercaCammino(self, e):
+        pass
+
+    def _fillDDProds(self):
         pass
 
 
--- model/model.py
+++ model/model.py
@@ -1,9 +1,48 @@
+import networkx as nx
+
 from database.DAO import DAO
 
 
 class Model:
     def __init__(self):
-        pass
+        self._graph = nx.DiGraph()
+        self._products = []
+        self._idMap = {}
+        self._idMapCat = {}
 
     def getDateRange(self):
         return DAO.getDateRange()
+
+    def getCategories(self):
+        return DAO.getAllCat()
+
+    def buildGraph(self, cat, date1, date2):
+        self._graph.clear()
+        for c in DAO.getAllCat():
+            self._idMapCat[c.category_name] = c
+        self._products = DAO.getAllProductsbyCategory(cat, self._idMapCat)
+        for p in self._products:
+            self._idMap[p.product_id] = p
+
+        self._graph.add_nodes_from(self._products)
+
+        allEdges = DAO.getEdges(cat, date1, date2, self._idMap, self._idMapCat)
+        for e in allEdges:
+            self._graph.add_edge(e[0], e[1], weight=e[2])
+
+    def getGraphDetails(self):
+        return self._graph.number_of_nodes(), self._graph.number_of_edges()
+
+    def getBestProdotti(self):
+        bestprodotti = []
+        for n in self._graph.nodes:
+            score = 0
+            for e_out in self._graph.out_edges(n, data=True):
+                score += e_out[2]["weight"]
+            for e_in in self._graph.in_edges(n, data=True):
+                score -= e_in[2]["weight"]
+
+            bestprodotti.append((n, score))
+
+        bestprodotti.sort(reverse=True, key=lambda x: x[1])
+        return bestprodotti[0:5]
